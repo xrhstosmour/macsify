e9e6454fcde9abbe3afd855d8eee5fc98ecabd69
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2026-09-09 18:10:08 +0300

    Add sleep keybinding via `AeroSpace`

--- .config/fish/functions/keybindings.fish
+++ .config/fish/functions/keybindings.fish
@@ -64,6 +64,7 @@ function keybindings_cheat_sheet --description "Display all keybindings with des
     echo "  - Key 2 + Space                  Switch input source"
     echo "  - Key 2 + .                      Spotlight search"
     echo "  - Key 2 + \\                      Lock screen"
+    echo "  - Key 2 + -                      Sleep"
     echo "  - Key 2 + Esc                    Shutdown"
     echo "  - Key 2 + ]                      Restart"
     echo "  - Key 3 + ,                      Reveal hidden menu bar icons"
