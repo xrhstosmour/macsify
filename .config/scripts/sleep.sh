24c30c481f0299531383e0c0964023b95679603e
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2026-09-14 10:27:36 +0300

    End Amphetamine session before sleep keybinding

--- .config/scripts/sleep.sh
+++ .config/scripts/sleep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# End any active Amphetamine session so it doesn't block sleep.
+osascript -e 'if application "Amphetamine" is running then tell application "Amphetamine" to end session' 2>/dev/null
+
 # Sleep the machine immediately.
 pmset sleepnow
